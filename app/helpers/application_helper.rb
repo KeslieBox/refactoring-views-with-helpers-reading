0a11cad7963c4f141ac291bb963a296bc7aec89d
KeslieBox <kesliebox@gmail.com>
2021-03-20 18:02:36 -0500

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def title(text)
+        #send our text to the place in our application layout 
+        #that is expecting some content for the :title.
+        content_for :title, text
+    end 
 end
